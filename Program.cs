7efeb5d0769c778ac38717bf5265ac1832aa9f19
Team3636 <32879830+Team3636@users.noreply.github.com>
2018-12-27 13:25:24 -0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FRCDriverStation
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(String[] args)
+        {
+            MessageBox.Show(args[1]);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
